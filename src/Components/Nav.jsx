4dce40489a0ad305590b619f25c38194132edc01
Yusuf <yusufmcgmcg@gmail.com>
2024-11-06 08:14:20 +0530

    Remove_Task_function_added

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,8 +3,8 @@ import { MdDelete } from "react-icons/md";
 import { ImMoveDown,ImMoveUp } from "react-icons/im";
 function Nav() {
 
-  const[task,settask]=useState(['Eat chick bun with macroni note that its not good for your helath though'])
-  const[newtask,setnewtask]=useState("")
+  const[tasks,settask]=useState(['Eat chick bun with macroni note that its not good for your helath though'])
+  const[newtasks,setnewtask]=useState("")
 
 
   function handleinput(event){
@@ -12,13 +12,19 @@ function Nav() {
     
   }
   function addtask(){
-      if(newtask.trim !== ""){
-        settask(task=>[...task,newtask])
-        setnewtask("")
-
+    if(newtasks.trim() !== ""){
+        settask(t=>[...t,newtasks]);
+        setnewtask("");
+    }
+    else{
+      alert("Please Enter Task !");
     }
   }
-  function deletetask(index){}
+  function deletetask(index){
+    const updatedtasks=(tasks => tasks.filter((_, i) => i !== index))
+    settask(updatedtasks);
+
+  }
   function mvtaskup(index){}
   function mvtaskdown(index){}
 
@@ -29,24 +35,24 @@ function Nav() {
           <input
             type="text"
             placeholder="Enter task..."
-            value={newtask}
+            value={newtasks}
             onChange={handleinput}
             className="rounded-md w-[500px] border border-stone-950 p-2 mr-14 ml-10"
           />
           <button className="ml-4 w-[100px]  bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"onClick={addtask}>Add</button>
       </div>
       <ol className='mt-14'>
-        {task.map((eletask,eleindex)=>
+        {tasks.map((eletask,eleindex)=>
             <li key={eleindex} className='flex py-5 justify-between w-[500px]'>
                 <span>{eleindex+1}. {eletask} </span> 
                 <span className='flex'>
-                  <button  className="rounded-xl text-xl text-black px-4 py-2  hover:bg-violet-600"onClick= {mvtaskup(eleindex)}>
+                  <button  className="rounded-xl text-xl text-black px-4 py-2  hover:bg-violet-600" onClick={() => mvtaskup(eleindex)}>
                     <ImMoveUp/>
                   </button>
-                  <button  className="rounded-xl text-xl text-black px-4 py-2  hover:bg-violet-600"onClick= {mvtaskdown(eleindex)}>
+                  <button  className="rounded-xl text-xl text-black px-4 py-2  hover:bg-violet-600" onClick={() => mvtaskdown(eleindex)}>
                     <ImMoveDown/>
                   </button>  
-                  <button  className="rounded-xl text-xl  text-black px-4 py-2  hover:bg-red-600"onClick={deletetask(eleindex)}>
+                  <button  className="rounded-xl text-xl  text-black px-4 py-2  hover:bg-red-600" onClick={() => deletetask(eleindex)}>
                     <MdDelete/>
                   </button>                                        
                 </span> 
